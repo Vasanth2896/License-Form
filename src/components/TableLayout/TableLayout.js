e9eb213c0161888fed00bdbaf05a0a452213c74a
vasanth_nayak <vasanth@vhitechlab.com>
2020-08-04 07:57:43 +0530

    Vasanth | Made Search Filter

--- src/components/TableLayout/TableLayout.js
+++ src/components/TableLayout/TableLayout.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './TableLayout.scss';
 import ReactTable from 'react-table-v6'
-import { Button, TextField, InputAdornment, IconButton } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { app_onChange, onCancel } from '../../store/appActions';
 import _ from 'lodash';
 import TableHeaderContent from "./TableHeaderContent/TableHeaderContent";
 import ActionComponent from "./ActionComponent/ActionComponent";
-
+import SearchBox from "./SearchBox/SearchBox";
 
 const TableLayout = (props) => {
-    const { history, state,onCancel } = props;
+    const { history, state, onCancel } = props;
     const currentState = _.cloneDeep(state);
     const { userList } = currentState;
     const [userTableState, setUserTableState] = useState({
@@ -26,8 +24,12 @@ const TableLayout = (props) => {
         stateSort: false,
         districtSort: false
     });
-    const { searchInput, usernameSort, mailIdSort, mobileNumberSort, professionSort, addressSort, stateSort, districtSort } = userTableState;
+    const { filteredData,
+        searchInput, usernameSort, mailIdSort, mobileNumberSort,
+        professionSort, addressSort, stateSort, districtSort } = userTableState;
     let newFilteredData;
+
+    console.log(userTableState);
 
     const handleSortChangeStyle = (props) => {
         if (props[0].id !== '') {
@@ -38,48 +40,79 @@ const TableLayout = (props) => {
         }
     }
 
+
     const handleSearchInputchange = (e) => {
         setUserTableState({ ...userTableState, searchInput: e.target.value });
     }
 
-    
+    useEffect(() => {
+        if (searchInput) {
+            globalSearchFilter();
+        }
+        else {
+            setUserTableState({ ...userTableState, filteredData: userList });
+        }
 
+    }, [searchInput]);
+
+
+    const globalSearchFilter = () => {
+        if (searchInput.length) {
+            newFilteredData = userList.filter(user => {
+                return (
+                    user.personalDetails.username.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.personalDetails.mailId.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.personalDetails.mobileNumber.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.professionalDetailToggle.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.addressDetails.communicationAddress.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.addressDetails.district.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    user.addressDetails.state.toLowerCase().includes(searchInput.toLowerCase())
+                );
+            });
+            if (newFilteredData.length) {
+                setUserTableState({ ...userTableState, filteredData: newFilteredData })
+            }
+            else if (!newFilteredData.length) {
+                setUserTableState({ ...userTableState, filteredData: [] });
+            }
+        }
+    }
 
     const columns = [
         {
             Header: <TableHeaderContent columnName='User name' columnSortState={usernameSort} />,
             id: 'username',
-            accessor: d => d.personalDetails.username
+            accessor: user => user.personalDetails.username
         },
         {
             Header: <TableHeaderContent columnName='Mail id' columnSortState={mailIdSort} />,
             id: 'mailId',
-            accessor: d => d.personalDetails.mailId
+            accessor: user => user.personalDetails.mailId
         },
         {
             Header: <TableHeaderContent columnName='Mobile no' columnSortState={mobileNumberSort} />,
             id: 'mobileNumber',
-            accessor: d => d.personalDetails.mobileNumber
+            accessor: user => user.personalDetails.mobileNumber
         },
         {
             Header: <TableHeaderContent columnName='Profession' columnSortState={professionSort} />,
             id: 'profession',
-            accessor: d => d.professionalDetailToggle
+            accessor: user => user.professionalDetailToggle
         },
         {
             Header: <TableHeaderContent columnName='Address' columnSortState={addressSort} />,
             id: 'address',
-            accessor: d => d.addressDetails.communicationAddress
+            accessor: user => user.addressDetails.communicationAddress
         },
         {
             Header: <TableHeaderContent columnName='District' columnSortState={districtSort} />,
             id: 'district',
-            accessor: d => d.addressDetails.district
+            accessor: user => user.addressDetails.district
         },
         {
             Header: <TableHeaderContent columnName='State' columnSortState={stateSort} />,
             id: 'state',
-            accessor: d => d.addressDetails.state
+            accessor: user => user.addressDetails.state
         },
         {
             Header: '',
@@ -103,48 +136,25 @@ const TableLayout = (props) => {
                     <h3>Individual Users</h3>
                     <p>({userList.length})</p>
                 </div>
-                <div className='searchbarContainer'>
-                    <div>
-                        <TextField
-                            label="Search"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment>
-                                        <IconButton>
-                                            <SearchIcon />
-                                        </IconButton>
-                                    </InputAdornment>
-                                )
-                            }}
-                            type='search'
-                            variant='outlined'
-                            onChange={(e) => handleSearchInputchange(e)}
-                            value={searchInput}
-                        />
-                    </div>
-                    <div>
-                        <Button
-                            color='primary'
-                            variant='contained'
-                            style={{ textTransform: 'unset' }}
-                            onClick={() => {
-                                history.push('/layout/PersonalDetails');
-                                onCancel();
-                            }}
-                        >+ New User</Button>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <ReactTable
-                    data={userList}
-                    columns={columns}
-                    className='-striped -highlight'
-                    minRows={0}
-                    onSortedChange={(props) => handleSortChangeStyle(props)}
-                    showPagination={false}
+                <SearchBox
+                    handleSearchInputchange={handleSearchInputchange}
+                    searchInput={searchInput}
+                    history={history}
+                    onCancel={onCancel}
                 />
             </div>
+            {!filteredData.length || !userList.length ? <div><h1>There is no story</h1></div> :
+                <div>
+                    <ReactTable
+                        data={filteredData}
+                        columns={columns}
+                        className='-striped -highlight'
+                        minRows={0}
+                        onSortedChange={(props) => handleSortChangeStyle(props)}
+                        showPagination={false}
+                    />
+                </div>
+            }
         </div>
     )
 }
